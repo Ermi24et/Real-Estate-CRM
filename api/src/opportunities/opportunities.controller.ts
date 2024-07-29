f106b2063d58973a9426832a92f42de552d8cfb5
Ermi24et <lilermi24@gmail.com>
2024-07-29 22:06:12 +0300

    opportunities of the crm

--- api/src/opportunities/opportunities.controller.ts
+++ api/src/opportunities/opportunities.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { OpportunitiesService } from './opportunities.service';
 import { CreateOpportunityDto } from './dto/create-opportunity.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Public } from 'src/auth/decorator/public.decorator';
 import { CommentsService } from 'src/comments/comments.service';
 
@@ -15,11 +15,17 @@ export class OpportunitiesController {
   ) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create opportunity' })
+  @ApiResponse({ status: 201, description: 'Opportunity created successfully' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   async create(@Body() createOpportunityDto: CreateOpportunityDto) {
     return this.opportunitiesService.create(createOpportunityDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get Opportunity by Id' })
+  @ApiResponse({ status: 200, description: 'returned opportunity by Id' })
+  @ApiResponse({ status: 404, description: 'Opportunity Not Found' })
   async findOne(
     @Param('id') id: string,
     @Query('cursor') cursor?: number,
