3e0516eaf6eddf5aa233e513be16adf2f7190031
Ermi24et <lilermi24@gmail.com>
2024-08-02 08:34:50 +0300

    entry point

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import {
   ClassSerializerInterceptor,
   Logger,
   ValidationPipe,
 } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
