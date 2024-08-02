279c2362ac1eb14f65ce96dfc6baa742aa3146da
Ermi24et <lilermi24@gmail.com>
2024-08-02 07:45:59 +0300

    docs(auth): added a comment in property decorator

--- api/src/auth/decorator/propertyId.decorator.ts
+++ api/src/auth/decorator/propertyId.decorator.ts
@@ -1,5 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+// property decorator
 export const PropertyId = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
